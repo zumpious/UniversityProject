0ee671e3e08b33b910b2ada318fb045a495fbb8d
zumpi <jonas@ostec.de>
2020-09-03 18:00:45 +0200

    Add Login default

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import Main from "./Main";
+import { View, Text } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 /*
 const theme = {
@@ -14,11 +16,36 @@ const theme = {
     }
 }
 */
-const App = () => {
+export function App() {
+    // Set an initializing state whilst Firebase connects
+    const [initializing, setInitializing] = useState(true);
+    const [user, setUser] = useState();
+
+    // Handle user state changes
+    function onAuthStateChanged(user) {
+        setUser(user);
+        if (initializing) setInitializing(false);
+    }
+
+    useEffect(() => {
+        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+        return subscriber; // unsubscribe on unmount
+    }, []);
+
+    if (initializing) return null;
+
+    if (!user) {
+        return (
+            <View>
+                <Text>Login</Text>
+            </View>
+        );
+    }
+
     return (
-        <PaperProvider>
-            <Main />
-        </PaperProvider>
+        <View>
+            <Text>Welcome {user.email}</Text>
+        </View>
     );
 }
 
