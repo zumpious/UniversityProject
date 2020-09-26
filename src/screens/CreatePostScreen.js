0c49e0e5cd1d28dc4272d9688d571beea68012ae
zumpi <jonas@ostec.de>
2020-09-26 23:15:10 +0200

    Add Title and description input

--- src/screens/CreatePostScreen.js
+++ src/screens/CreatePostScreen.js
@@ -1,9 +1,13 @@
-import React, {useState} from 'react';
-import { Text, View, PermissionsAndroid } from 'react-native';
-import {Button, Provider as PaperProvider} from "react-native-paper";
+import React, {useContext, useState} from 'react';
+import {Text, View, PermissionsAndroid, StyleSheet, TextInput} from 'react-native';
+import {Button, Provider as PaperProvider, Title} from "react-native-paper";
 import Geolocation from 'react-native-geolocation-service';
+import {AuthContext} from "../navigation/AuthNavigator";
+import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view";
 
 export function CreatePostScreen({ navigation }) {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
     const [position, setPosition] = useState({
         latitude: null,
         longitude: null,
@@ -48,21 +52,110 @@ export function CreatePostScreen({ navigation }) {
         }
     };
 
+    //ToDo think about a way that allows the user to easy delete the value of an input area (e.g. delete button inside the textInput)
     return (
-        <PaperProvider>
-            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <Button
-                    mode="contained"
-                    color="#788eec"
-                    labelStyle={{color: "white"}}
-                    onPress={getLocationPermissionAndCoordinates}>
-                    Get Location
-                </Button>
-                <Text>{position.latitude}</Text>
-                <Text>{position.longitude}</Text>
-            </View>
-        </PaperProvider>
+        <View style={styles.container}>
+            <KeyboardAwareScrollView
+                style={{ flex: 1, width: '100%' }}
+                keyboardShouldPersistTaps="always">
+
+                <Title style={styles.title}>Title</Title>
+                <TextInput
+                    style={styles.input}
+                    placeholder='Title'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setTitle(text)}
+                    value={title}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                />
+
+                <Title style={styles.title}>Description</Title>
+                <TextInput
+                    style={[styles.input, styles.description]}
+                    placeholder='Description'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setDescription(text)}
+                    value={description}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                    multiline = {true}
+                    numberOfLines = {5}
+                />
+
+                <View style={{flex: 1, alignItems: 'center'}}>
+                    <Button
+                        mode="contained"
+                        color="#788eec"
+                        labelStyle={{color: "white"}}
+                        onPress={getLocationPermissionAndCoordinates}>
+                        Get Location
+                    </Button>
+                    <Text>{position.latitude}</Text>
+                    <Text>{position.longitude}</Text>
+                </View>
+
+            </KeyboardAwareScrollView>
+        </View>
     );
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    title: {
+        borderRadius: 5,
+        overflow: 'hidden',
+        marginTop: 25,
+        marginLeft: 30,
+        marginRight: 30,
+        marginBottom: 5
+    },
+    input: {
+        height: 48,
+        borderRadius: 5,
+        overflow: 'hidden',
+        backgroundColor: 'white',
+        marginTop: 5,
+        marginBottom: 10,
+        marginLeft: 30,
+        marginRight: 30,
+        paddingLeft: 16
+    },
+    description: {
+        height: 96,
+        marginTop: 10
+    },
+    button: {
+        backgroundColor: '#788eec',
+        marginLeft: 30,
+        marginRight: 30,
+        marginTop: 20,
+        height: 48,
+        borderRadius: 5,
+        alignItems: "center",
+        justifyContent: 'center'
+    },
+    buttonTitle: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: "bold"
+    },
+    footerView: {
+        flex: 1,
+        alignItems: "center",
+        marginTop: 20
+    },
+    footerText: {
+        fontSize: 16,
+        color: '#2e2e2d'
+    },
+    footerLink: {
+        color: "#788eec",
+        fontWeight: "bold",
+        fontSize: 16
+    }
+});
 
 export default CreatePostScreen;
